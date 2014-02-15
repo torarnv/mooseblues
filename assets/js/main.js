9fca8b1fd0d5e439463acd342553279954bb04d4
Tor Arne Vestbø <torarnv@gmail.com>
2014-02-16 00:03:46 +0100

    Add signup form

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,69 @@
 				$('body').toggleClass('body-push-toleft');
 				$('#theMenu').toggleClass('menu-open');
 			});
+
+			$('#signup-form').validate({
+				submitHandler: function(form) {
+
+					var mappings = {
+						 'firstname' 	: 'fname',
+						 'lastname' 	: 'lname' ,
+						 'city' 		: 'atr.563974' ,
+						 'country' 		: 'atr.563975' ,
+						 'email' 		: 'email' ,
+						 'role' 		: 'atr.563976' ,
+						 'ticket' 		: 'price' ,
+					};
+
+					for (var field in mappings)
+						$("*[name*='" + field + "']").attr('name', "$" + mappings[field]);
+				
+					if (window.location.hostname == "www.mooseblues.no")
+						var redirect = "https://deltager.no/moose_blues_2014"
+					else
+						var redirect = "deltager.html"
+
+					window.location.href = redirect + "#" + $(form).serialize(); 
+				},
+			  rules: {
+			    firstname: {
+			      required: true
+			    },
+			    lastname: {
+			      required: true
+			    },
+			    city: {
+			      required: true
+			    },
+			    country: {
+			      required: true
+			    },
+			    email: {
+			      required: true,
+			      email: true
+			    },
+			    role: {
+			      required: true,
+			    },
+			    ticket: {
+			      required: true,
+			    }
+			  },
+			  errorClass: "form-error",
+			  highlight: function(element) {
+			    $(element).closest('.form-group').addClass('has-error');
+			  },
+			  success: function(element) {
+			    element.closest('.form-group').removeClass('has-error');
+			  }
+			 });
+
+			$("#ticket-selector").change(function() {
+			  	$('#signup-form').valid();
+			});
+
+			$("#signup-form").sisyphus();
+
+			if (window.location.hash.indexOf("testsignup") != -1)
+				$("#signup-button").removeAttr("disabled");
 })(jQuery)
